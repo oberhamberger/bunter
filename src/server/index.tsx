4e71c91b6ca7baa2054511afb6cb41a81c243911
Christian Oberhamberger <c.oberhamberger@gmail.com>
2023-11-10 13:40:18 +0100

    added 404 response

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -4,15 +4,20 @@ import Index from './template/Index.html';
 
 export default () => {
     const app = express();
-    app.use(express.static('dist'));
 
-    app.use((req, res) => {
+    app.use(express.static('dist'));
+    app.get('/', (req, res) => {
         const markup = renderToString(<Index />);
         console.info(`Rendered App for ${req.url}`);
 
         res.setHeader('Content-Type', 'text/html; charset=utf-8');
         res.status(200);
         res.send(markup);
+    });
+    app.use((req, res) => {
+        console.error(`Didn't find anything for: ${req.url}`);
+        res.status(404);
+        res.send(`Didn't find anything for you here`);
     });
 
     const server = app.listen(3030, () => console.info(`Server started at http://localhost:3030`));
